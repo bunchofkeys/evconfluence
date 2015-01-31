3be51d5861bdd80a45d5b66259236a42e1c9cab8
Fang Liang <foofangliang87@gmail.com>
2015-01-31 11:31:19 +0800

    update db

--- app/config/database.php
+++ app/config/database.php
@@ -54,11 +54,11 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '$OPENSHIFT_MYSQL_DB_HOST',
-			'port'		=> '$OPENSHIFT_MYSQL_DB_PORT',
-			'database'  => 'app',
-			'username'  => 'admin7nZK477',
-			'password'  => 'gLak3gmWcW1h',
+			'host'      => getenv('OPENSHIFT_MYSQL_DB_HOST'),
+			'port'		=> getenv('OPENSHIFT_MYSQL_DB_PORT'),
+			'database'  => getenv('OPENSHIFT_APP_NAME'),
+			'username'  => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+			'password'  => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
